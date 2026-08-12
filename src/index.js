3adfff1c7d8990b0fe77f421570cc33d1513f77c
TheOzco <so.sayyahi@gmail.com>
2026-08-12 12:30:09 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { connect } from "cloudflare:sockets";
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
 
-const proxyIPs = ["104.18.47.25"];
+const proxyIPs = ["172.64.159.184"];
 const cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
